5bcabdf0049a8a6a4758a43dd5ae85545aeff293
Silver_Rose <130399374+sofiabrussa123@users.noreply.github.com>
2024-06-25 13:56:05 -0300

    primer commit

---
+++ Evaluacion_2.cpp
@@ -0,0 +1,40 @@
+/******************************************************************************
+Sofia Brussa 5° 2da
+compilador "https://www.onlinegdb.com/"
+Tema N°5
+
+El club del barrio inaugura un gimnasio en sus instalaciones. En los primeros meses deciden promocionar solamente 4 clases, 
+estas son "Top Ride", "Zumba", "Entrenamiento" y "Abdominales". Cada clase tiene un cupo de participantes. 
+Todas las clases se dan en simultáneo y tienen 5 horarios diferentes. 
+Estos horarios se distinguen como Horario 1, Horario 2. Horario 3, Horario 4 y Horario 5.
+Para llevar un control en el gimnasio los alumnos deben anotarse 
+cada vez que ingresan a una clase dando como dato el "Nombre" de dicha clase; 
+aquellos que superen el cupo se los debe informar a través de un mensaje
+Puede ocurrir que quede algún horario sin el dictado de alguna de las actividades por no asistir ningún alumno,
+Los informes se deben dar cuando el usuario no quiera ingresar más alumnos porque los horarios finalizaron 
+o porque todas las clases en sus horarios carecen de cupo.
+
+Se pide:
+Utilizar un vector para el cupo de las actividades y una matriz para registrar la asistencia de los alumnos
+Informar cual es la clase en la que no concurrió ningún alumno
+Informar cual es el horario en el que no se dicto ninguna clase
+Informar la cantidad de personas que concurrió a cada clase
+informar la ganancia obtenida en el dia sabiendo que en el horario central la clase cuesta 5 700 
+y en los horarios inferiores el costo disminuye en un 20% y en los horarios superiores el costo aumenta un 15%
+
+clases (se dan el simultaneo)
+top Ride
+Zumba 
+Entrenamiento
+Abdominales
+
+horarios 
+1
+2
+3
+4
+5
+
+inscripcion (cada clase)
+nombre de alumno
+*******************************************************************************/
